8d574f2ac9b30b9f5fd2a05d5f388ac0ab5b1f08
Nick Chernyak <64402886+Nick-Chernyak@users.noreply.github.com>
2025-03-05 21:48:52 +0100

    init

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+    site: 'https://Kotyane.github.io/reviews/',
+    base: '/kotyane-reviews/',
+    output: 'static',
+});
