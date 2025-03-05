79a06fe14847a994673b5d95d2b7cc7f1f1c9b96
Nick Chernyak <64402886+Nick-Chernyak@users.noreply.github.com>
2025-03-05 21:49:02 +0100

    added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://Kotyane.github.io/reviews/',
     base: '/kotyane-reviews/',
     output: 'static',
 });
